84c46daa1c35eace408f231f1673051d58df5b3b
juan25 <elbartoy@gmail.com>
2025-09-11 21:36:47 -0600

    Emaquetado de la pagina principal

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galleria Dibujo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-top">
+                <a href="#" class="logo">Tecnicas Dibujo</a>
+                <input type="checkbox" id="menu">
+                <label for="menu">
+                    <img src="img/menu.png" class="menu-icono" alt="menu">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="#"> Inicio </a></li>
+                        <li><a href="#"> Aprender </a></li>
+                        <li><a href="#"> Tecnicas </a></li>
+                        <li><a href="#"> Estilos </a></li>
+                    </ul>
+                </nav>
+            </div>
+      
+
+        <div class="header-content container">
+            <div class="header-txt"> 
+                <h1>Galleria Dibujo</h1><br>
+                <h2>¿Como Aprender a dibujar?</h2>
+                <p>El dibujo es un lenguaje visual que todos podemos aprender. Empezar es más sencillo de lo que crees: solo necesitas un lápiz, papel y muchas ganas. Con prácticas constante descubiraras que dibujar no se trata de hacerlo perfecto, sino de expresar ideas y mejorar paso a paso</p>
+                <a href="#" class="btn-1">Informacion</a>
+            </div>
+            <div class="header-img">
+                <img src="img/about.png" alt="Imagen de dibujo">
+        </div>
+    </div>
+
+
+    </header>
+
+    <section class="learning container">
+        <div class="learning-img">
+            <img src="img/learnin.jpg" alt="Aprendiendo a dibujar">
+        </div>
+
+        <div class="learnin-txt">
+            <h2> ¿Lo que podemos Aprender? </h2>
+            <p>
+                Para comenzar a dibujar lo más importante es aprender a observar. 
+                Iniciaremos con ejercicios simples de líneas, formas y figuras básicas, 
+                ya que todo dibujo está formado por ellas. Practicarás círculos, cuadrados y triángulos, 
+                que son la base de cualquier ilustración.
+            </p>
+            <br>
+            <p>
+                Después, iremos paso a paso hacia el trazo de objetos sencillos, 
+                el uso de sombras para dar volumen y cómo equilibrar tus dibujos. 
+                Lo esencial no es la perfección, sino disfrutar el proceso y mejorar con cada intento.
+            </p>
+        </div>
+    </section>
+
+    <main class="servicios">
+        <h2>Técnicas Básicas de Dibujo</h2>
+        <div class="servicios-content container">
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-pencil"></i>
+                <h3>Dibujo a Lápiz</h3>
+                <p>Comienza con lo esencial: trazos, figuras básicas y sombreado para dar volumen.</p>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-fill-drip"></i>
+                <h3>Acuarela</h3>
+                <p>Explora cómo mezclar colores y crear efectos suaves con agua y pincel.</p>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-paintbrush"></i>
+                <h3>Acrílicos</h3>
+                <p>Aprende a usar la pintura acrílica para lograr colores intensos y expresivos.</p>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-hand-sparkles"></i>
+                <h3>Soltar la Mano</h3>
+                <p>Ejercicios sencillos para mejorar tu confianza y fluidez al dibujar.</p>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-tablet-screen-button"></i>
+                <h3>Dibujo Digital</h3>
+                <p>Introduce el uso de tabletas gráficas y programas básicos para crear arte digital.</p>
+            </div>
+
+        </div>
+    </main>
+    
+    <footer class="footer">
+        <div class="footer-content container">
+            <div class="link">
+                <a href="#" class="logo">Tecnicas Dibujo</a>
+            </div>
+
+            <div class="link">
+                  <ul>
+                    <li><a href="#"> Inicio </a></li>
+                    <li><a href="#"> Aprender </a></li>
+                    <li><a href="#"> Tecnicas </a></li>
+                    <li><a href="#"> Estilos </a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+     <?php
+        include("send.php");
+     ?>
+
+
+     <scrip>
+       
+     </scrip>
+
+
+
+</body>
+</html>
